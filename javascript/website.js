3b086a939bcb0981f1b969005caeb08bbcca85c1
Bruce Webber <self@brucewebber.us>
2014-09-14 16:44:15 -0400

    Added smooth scrolling.

--- javascript/website.js
+++ javascript/website.js
@@ -1,2 +1,18 @@
 // Javascript for michiganbitcoiners.org
 
+$(document).ready(function(){
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('html,body').animate({
+                     scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+        }
+    });
+});
